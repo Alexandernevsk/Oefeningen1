ef66cd85ea749198f49de26a8b878afbcd8e4bcb
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 14:27:01 +0100

    initial commit

--- src/Hallo/Main.java
+++ src/Hallo/Main.java
@@ -6,5 +6,9 @@ public class Main {
         System.out.println("veranderen in monti");
         System.out.println("Odwars gelooft dat iemand een spion is");
         System.out.println("Odward gelooft dat er een individu is, waarvan die een spion is.");
+
+
+
+
     }
 }
