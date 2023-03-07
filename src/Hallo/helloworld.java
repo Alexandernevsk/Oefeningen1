09ce47fecbdf1d72ed2f8102bea34628c74eb6fe
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 13:12:39 +0100

    initial commit

--- src/Hallo/helloworld.java
+++ src/Hallo/helloworld.java
@@ -2,5 +2,6 @@ package Hallo;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("No parlo el Catalan, s'ils plau parla Castellan con meu");
+        System.out.println("Merde");
     }
 }
