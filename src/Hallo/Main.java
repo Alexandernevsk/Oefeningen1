5c9b6f90769ec85a44eb74eaad2da51f1101055e
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 14:20:11 +0100

    initial commit

--- src/Hallo/Main.java
+++ src/Hallo/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("ja zo dus");
+        System.out.println("Niemand weet Niemand weet, hoe deze website heet");
+        System.out.println("Lasciare ogni sospetto, ogni nio vulta.");
     }
 }
