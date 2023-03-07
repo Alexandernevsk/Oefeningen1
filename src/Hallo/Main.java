d03557078281b566c3b41e44e2335c757fa06b7b
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 13:25:10 +0100

    initial commit

--- src/Hallo/Main.java
+++ src/Hallo/Main.java
@@ -3,6 +3,6 @@ package Hallo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Ah nee the menselijkheid");
+        System.out.println("Ah nee the menselijkheid, ah neeeee");
     }
 }
