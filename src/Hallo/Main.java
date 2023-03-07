bae184ca5a7a91323f81fd031d60ffe43f07b01c
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 14:42:44 +0100

    initial commit

--- src/Hallo/Main.java
+++ src/Hallo/Main.java
@@ -2,8 +2,6 @@ package Hallo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("ja zo dus");
         System.out.println("Niemand weet Niemand weet, hoe deze website heet");
         System.out.println("Lasciare ogni sospetto, ogni nio vulta.");
         System.out.println("Lasciare ogni sospetto, ogni nio vulta.");
