b03270c642fdcc2e8610e34a7f497df270df722d
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 12:44:21 +0100

    initial commit

---
+++ src/Hallo/Main.java
@@ -0,0 +1,7 @@
+package Hallo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Hallo/helloworld.java
@@ -0,0 +1,4 @@
+package Hallo;
+
+public class helloworld {
+}
