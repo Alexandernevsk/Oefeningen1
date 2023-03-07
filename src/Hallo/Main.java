ae3dce1d31f3a58569284339b4cae4970896a27c
Alexandernevsk <Alexandertorreman@gmail.com>
2023-03-07 14:24:50 +0100

    initial commit

--- src/Hallo/Main.java
+++ src/Hallo/Main.java
@@ -3,6 +3,8 @@ package Hallo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Lets check");
+        System.out.println("veranderen in monti");
+        System.out.println("Odwars gelooft dat iemand een spion is");
+        System.out.println("Odward gelooft dat er een individu is, waarvan die een spion is.");
     }
 }
